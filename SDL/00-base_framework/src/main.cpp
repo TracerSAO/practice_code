089cc71cdf1fdffbe043671c2add22e660ca6943
tracersao <tracersao@163.com>
2025-07-03 19:26:17 +0800

    update 00-base_framework
    Signed-off-by: tracersao <tracersao@163.com>

--- SDL/00-base_framework/src/main.cpp
+++ SDL/00-base_framework/src/main.cpp
@@ -1,8 +1,9 @@
 #define SDL_MAIN_USE_CALLBACKS 1
-
 #include <SDL3/SDL_main.h>
 
-#include "SDL_workflow.hpp"
+#include <stdexcept>
+
+#include "App.hpp"
 
 extern "C" {
 
@@ -11,27 +12,50 @@ SDL_AppResult SDLCALL SDL_AppInit(void **appstate, int argc, char *argv[])
     (void)appstate;
     (void)argc;
     (void)argv;
-    return SDL::SDL_Workflow::onInit(argc, argv);
+
+    try {
+        App::Create();
+    }
+    catch (const std::runtime_error &error) {
+        return SDL_AppResult::SDL_APP_FAILURE;
+    }
+    catch (...) {
+        return SDL_AppResult::SDL_APP_FAILURE;
+    }
+
+    return SDL_AppResult::SDL_APP_CONTINUE;
 }
 
 void SDLCALL SDL_AppQuit(void *appstate, SDL_AppResult result)
 {
     (void)appstate;
     (void)result;
-    SDL::SDL_Workflow::onQuit(result);
+
+    App::Destory();
 }
 
 SDL_AppResult SDLCALL SDL_AppIterate(void *appstate)
 {
     (void)appstate;
-    return SDL::SDL_Workflow::onUpdate();
+
+    App::Render();
+
+    return SDL_AppResult::SDL_APP_CONTINUE;
 }
 
 SDL_AppResult SDLCALL SDL_AppEvent(void *appstate, SDL_Event *event)
 {
     (void)appstate;
-    (void)event;
-    return SDL::SDL_Workflow::onEvent(event);
+
+    switch (event->type)
+    {
+        case SDL_EVENT_QUIT: {
+            return SDL_APP_SUCCESS;
+        }
+        default: {
+            return SDL_AppResult::SDL_APP_CONTINUE;
+        }
+    }
 }
 
 } // extern "C"
